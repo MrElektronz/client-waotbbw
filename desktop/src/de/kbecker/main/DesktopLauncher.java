e7a2baf442f9eba5f5e5ba643700832828cd46fb
KBeck <mrelektronz@gmail.com>
2022-05-06 17:28:18 +0200

    initial commit

---
+++ desktop/src/de/kbecker/main/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package de.kbecker.main;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import de.kbecker.main.ClientMain;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("BBW");
+		new Lwjgl3Application(new ClientMain(), config);
+	}
+}
